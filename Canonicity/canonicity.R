e2cd7e41a62dcacfc7c21a4fa08699e50b3357b6
Sean Fischer <sf585978@gmail.com>
2015-02-10 14:22:00 -0500

    Added Non-Canon Texts
    Included non-canon texts, created non-canon dtm, and then compared word
    length between the two groups.

--- Canonicity/canonicity.R
+++ Canonicity/canonicity.R
@@ -12,9 +12,15 @@ require(Rgraphviz)
 
 ##### Load Canon Corpus Data #####
 
-cname <- file.path("corpus/canon")
+cname.1 <- file.path("corpus/canon")
 
-canon <- Corpus(DirSource(cname))
+canon <- Corpus(DirSource(cname.1))
+
+##### Load Non-Canon Corpus Data #####
+
+cname.2 <- file.path("corpus/noncanon/")
+
+noncanon <- Corpus(DirSource(cname.2))
 
 ##### Preparing Our Canon Corpus #####
 
@@ -28,11 +34,56 @@ canon <- tm_map(canon, removeWords, stopwords("English"))
 canon <- tm_map(canon, stripWhitespace) # remove whitespace
 canon <- tm_map(canon, stemDocument) # remove common endings
 
+##### Preparing Our Non-Canon Corpus #####
+
+noncanon <- tm_map(noncanon, toSpace, "/|@|\\|")
+noncanon <- tm_map(noncanon, content_transformer(tolower)) # set to lowercase
+noncanon <- tm_map(noncanon, removeNumbers) # remove numbers
+noncanon <- tm_map(noncanon, removePunctuation) # remove punctuation
+noncanon <- tm_map(noncanon, removeWords, stopwords("English")) 
+noncanon <- tm_map(noncanon, stripWhitespace) # remove whitespace
+noncanon <- tm_map(noncanon, stemDocument) # remove common endings
+
 ##### Canon Document Term Matrix #####
 
 canonDTM <- DocumentTermMatrix(canon) # create a document term matrix
-freq <- colSums(as.matrix(canonDTM)) # word frequencies
-ord <- order(freq) # ordered least to greatest
+freq.1 <- colSums(as.matrix(canonDTM)) # word frequencies
+ord.1 <- order(freq) # ordered least to greatest
 canonDTM.2 <- removeSparseTerms(canonDTM, 0.1) # remove sparse terms
+
+##### Non-Canon Document Term Matrix #####
+
+noncanonDTM <- DocumentTermMatrix(canon) # create a document term matrix
+freq.2 <- colSums(as.matrix(canonDTM)) # word frequencies
+ord.2 <- order(freq) # ordered least to greatest
+noncanonDTM.2 <- removeSparseTerms(canonDTM, 0.1) # remove sparse terms
+
+##### Quantitative Analysis Preparation #####
+
+canonWords <- canonDTM.2 %>%
+  as.matrix %>%
+  colnames %>%
+  (function(x) x[nchar(x) <20])
+
+noncanonWords <- noncanonDTM.2 %>%
+  as.matrix %>%
+  colnames %>%
+  (function(x) x[nchar(x) <20])
+
+##### Word Length #####
+
+data.frame(nletters.1 = nchar(canonWords)) %>%
+  ggplot(aes(x = nletters)) +
+  geom_histogram(binwidth = 1) +
+  geom_vline(xintercept = mean(nchar(canonWords)), colour = "green", size = 1,
+             alpha = .5) +
+  labs(x = "Number of Letters", y = "Number of Words")
+
+data.frame(nletters.2 = nchar(noncanonWords)) %>%
+  ggplot(aes(x = nletters)) +
+  geom_histogram(binwidth = 1) +
+  geom_vline(xintercept = mean(nchar(noncanonWords)), colour = "green", size = 1,
+             alpha = .5) +
+  labs(x = "Number of Letters", y = "Number of Words")
 
 
